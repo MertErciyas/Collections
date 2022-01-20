8491c38344934fdd5902a7d85a3d626db568e674
Mert Erciyas <99058235@mydavinci.nl>
2022-01-20 11:34:39 +0100

    Eminem

---
+++ Opdrachten/M&M_list.py
@@ -0,0 +1,12 @@
+from random import randint
+
+eminem = ['rood','orange','groen','blauw','geel','bruin']
+eminemBag = []
+
+def bagOfEminem(amountEminem):
+    for i in range(amountEminem): 
+        eminemBag.append(eminem[randint(0,len(eminem)-1)])
+    print(*eminemBag,sep=', ')
+
+ask = int(input("-------------------------------\nHow many Eminems would you like?\n-------------------------------\n"))
+bagOfEminem(ask)
