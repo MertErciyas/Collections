145b672fa091c85bc0bad51b7366d00fd32d4bf5
Mert Erciyas <99058235@mydavinci.nl>
2022-01-25 11:00:35 +0100

    Eminem

--- Opdrachten/EMINEM_SUPER_EDITION.py
+++ Opdrachten/EMINEM_SUPER_EDITION.py
@@ -1,18 +1,19 @@
 import random
 
-eminem = ['oranje', 'blauw', 'groen', 'bruin']
+colors = ['orange', 'blue', 'green', 'brown', 'red']
 
 
 def getEminemList(amount: int) -> list[str]:
-    return [eminem[random.randrange(0, len(eminem))] for _ in range(eminem)]
+    return [colors[random.randrange(0, len(colors))] for _ in range(amount)]
+    getEminemList.sort(reverse=True)
 
 
 def getEminemDict(amount: int) -> dict[str, int]:
     dictionary = {}
 
     for i in range(amount):
-        randomIndex = random.randrange(0, len(eminem))
-        color = eminem[randomIndex]
+        randomIndex = random.randrange(0, len(colors))
+        color = colors[randomIndex]
 
         if color in dictionary:
             dictionary[color] += 1
@@ -21,11 +22,8 @@ def getEminemDict(amount: int) -> dict[str, int]:
 
     return dictionary
 
-
 def sortMMS(collection):
     return sorted(collection)
-
-
 if __name__ == '__main__':
     print(getEminemList(int(input('How many Eminems\'s do you want?\n'))))
     print(getEminemDict(int(input('How many Eminems\'s do you want?\n')))) 
